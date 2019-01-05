5f0289a0f51f82a74f221acc0dcdb689491e2030
Jason Kao <jasonkao85@gmail.com>
2019-01-05 17:57:42 +0100

    finance

--- notebooks/inflation.py
+++ notebooks/inflation.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
+# Source: https://www.usinflationcalculator.com/inflation/consumer-price-index-and-annual-percent-changes-from-1913-to-2008/
 cpiData = pd.read_csv('../data/ConsumerPriceIndexData.csv').set_index('Year')
 
 # Returns a function that adjusts nominal dollars to [month] 2016 dollars.
-def inflationAdjuster(month):
-    finalCpi = cpiData.loc[2016][month]
+def inflationAdjuster(month, baseYear=2016):
+    finalCpi = cpiData.loc[baseYear][month]
     def adjust(dollars, year):
         if dollars is None:
             return
         initCpi = cpiData.loc[year][month]
         return dollars * finalCpi / initCpi
     return adjust
-
