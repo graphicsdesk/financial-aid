3fcbad0e0bd44eef35a98be2b7362ba78ebe673f
Jason Kao <jasonkao85@gmail.com>
2019-01-06 19:45:45 +0100

    bad coding

--- npt_debt.sh
+++ npt_debt.sh
@@ -3,7 +3,8 @@
 for year in {9..16}
 do
   yr="$( printf '%02d' "$year" )"
+  actual="$( printf '%02d' "$((year-1))" )"
   nxt="$( printf '%02d' "$((year+1))" )"
-  echo "20${yr}"
+  echo "20$actual"
   csvcut -c 4,NPT41_PRIV,NPT42_PRIV,NPT43_PRIV,COSTT4_A,PCTPELL data/scorecard/MERGED20${yr}_${nxt}_PP.csv | csvgrep -c 1 -m 'Columbia University in the City of New York' | csvlook
 done
