ce5aec765512ea06e57f3aeda1314d7447e9bdb3
Jason Kao <jk4248@columbia.edu>
2019-02-14 14:40:50 -0500

    barnard

--- npt_debt.sh
+++ npt_debt.sh
@@ -6,5 +6,5 @@ do
   actual="$( printf '%02d' "$((year-1))" )"
   nxt="$( printf '%02d' "$((year+1))" )"
   echo "20$actual"
-  csvcut -c 4,NPT41_PRIV,NPT42_PRIV,NPT43_PRIV,COSTT4_A,PCTPELL data/scorecard/MERGED20${yr}_${nxt}_PP.csv | csvgrep -c 1 -m 'Columbia University in the City of New York' | csvlook
+  csvcut -c 4,NPT41_PRIV,NPT42_PRIV,NPT43_PRIV,NPT44_PRIV,NPT45_PRIV,PCTPELL data/scorecard/MERGED20${yr}_${nxt}_PP.csv | csvgrep -c 1 -m 'Barnard College' | csvlook
 done
